ade6024933d66e7a7bbe74641d9ff0d5e191c9e2
Juan David <gottygarcia@gmail.com>
2020-03-12 01:08:55 -0500

    avances en accordeon

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const accordion = document.getElementsByClassName('accordion')
+const contenido = document.getElementsByClassName('contenido-conatiner')
 
 for (let i = 0; i < accordion.length; i++) {
 	accordion[i].addEventListener('click', () => {
 		if (accordion[i].children[1].innerText === '+') {
 			accordion[i].children[1].innerText = '-'
 			accordion[i].nextElementSibling.style.height = '300px'
+			contenido[i].style.opacity = '1'
 		} else {
 			accordion[i].children[1].innerText = '+'
 			accordion[i].nextElementSibling.style.height = '0px'
+			contenido[i].style.opacity = '0'
 		}
 	})
 }
