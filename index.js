b54f12bd2f2ac643ab5ec156fae317aa084d3009
Juan David <gottygarcia@gmail.com>
2020-03-13 01:50:47 -0500

    Modal terminado :D

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ function plusDivs(n) {
 }
 
 function showDivs(n) {
-	var i
 	var x = document.getElementsByClassName('mySlides')
+	var i
 	if (n > x.length) {
 		slideIndex = 1
 	}
@@ -35,8 +35,76 @@ function showDivs(n) {
 	}
 	for (i = 0; i < x.length; i++) {
 		x[i].style.display = 'none'
-		x[i].style.opacity = '0'
 	}
 	x[slideIndex - 1].style.display = 'flex'
-	x[slideIndex - 1].style.opacity = '1'
+}
+
+//Modal
+const Personas = [
+	{
+		nombre: 'Danna García',
+		profesion: 'UI/UX designer',
+		academico: 'Pontificia Universidad Javeriana - 2004',
+		laboral: 'Google - 2016',
+		img: './img/1.jpg'
+	},
+	{
+		nombre: 'Juan García',
+		profesion: 'Frontend Developer',
+		academico: 'Universidad Santiago de Cali - 2005',
+		laboral: 'Facebook - 2017',
+		img: './img/2.jpg'
+	},
+	{
+		nombre: 'Sofia Collazos',
+		profesion: 'Administradora de bases de datos',
+		academico: 'Universidad Libre - 2004',
+		laboral: 'Youtube - 2018',
+		img: './img/3.jpg'
+	},
+	{
+		nombre: 'David Rincón',
+		profesion: 'Backend Developer ',
+		academico: 'Universidad Icesi - 2003',
+		laboral: 'Instagram - 2019',
+		img: './img/4.jpg'
+	},
+	{
+		nombre: 'Kevin Castro',
+		profesion: 'Devops engineer',
+		academico: 'Universidad Autónoma de Occidente - 2006',
+		laboral: 'Twitter - 2015',
+		img: './img/5.jpg'
+	}
+]
+
+const modal_person = document.getElementById('modal')
+const back_modal = document.getElementById('modal-back')
+const back_modal_transparent = document.getElementById('modal-back-tranparent')
+back_modal_transparent.addEventListener('click', cerrarModal)
+
+const nombre = document.getElementById('nombre')
+const profesion = document.getElementById('profesion')
+const academico = document.getElementById('academico')
+const laboral = document.getElementById('laboral')
+const profile_img = document.getElementById('profile_img')
+
+function modal(persona) {
+	modal_person.style.zIndex = '15'
+	back_modal.style.opacity = '1'
+	back_modal.style.zIndex = '10'
+	back_modal_transparent.style.zIndex = '12'
+
+	nombre.innerText = Personas[persona].nombre
+	profesion.innerText = Personas[persona].profesion
+	academico.innerText = Personas[persona].academico
+	laboral.innerText = Personas[persona].laboral
+	profile_img.src = Personas[persona].img
+}
+
+function cerrarModal() {
+	modal_person.style.zIndex = '4'
+	back_modal.style.opacity = '0'
+	back_modal.style.zIndex = '4'
+	back_modal_transparent.style.zIndex = '4'
 }
