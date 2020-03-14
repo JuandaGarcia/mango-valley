040b49147961ee6916cbf5d006916b6da3b89dd0
Juan David <gottygarcia@gmail.com>
2020-03-13 21:28:38 -0500

    casi listo

--- index.js
+++ index.js
@@ -38,6 +38,26 @@ function showDivs(n) {
 	}
 	x[slideIndex - 1].style.display = 'flex'
 }
+showDivs2(slideIndex)
+
+function plusDivs2(n) {
+	showDivs2((slideIndex += n))
+}
+
+function showDivs2(n) {
+	var x = document.getElementsByClassName('mySlidesProjects')
+	var i
+	if (n > x.length) {
+		slideIndex = 1
+	}
+	if (n < 1) {
+		slideIndex = x.length
+	}
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = 'none'
+	}
+	x[slideIndex - 1].style.display = 'flex'
+}
 
 //Modal
 const Personas = [
